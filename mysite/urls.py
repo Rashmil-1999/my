27623100b7d7fe7cb9e9b2d2b32ca4750f2c919e
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2018-03-25 13:46:38 +0530

    added using urlfunc.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', include('blog.urls')),
+    url(r'', include('blog.urls')),
 ]
