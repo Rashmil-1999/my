a1e922e8a6968ceb784f8ba1e20e2bf10f064062
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2018-03-29 13:51:48 +0530

    latest post comes up feature added

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
     return render(request,'blog/post_list.html',{'posts': posts})
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk = pk)
@@ -36,4 +36,4 @@ def post_edit(request,pk):
             return redirect('post_detail', pk = post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form' : form})            
+    return render(request, 'blog/post_edit.html', {'form' : form})
