dc23d7a1022dbfe00a6fed276fea641211b89029
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2018-04-19 13:07:08 +0530

    added comment model

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.post_list, name = 'post_list'),
@@ -10,5 +11,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_remove, name='post_remove'),
     url(r'^post/new/publish/$',views.post_publish_new, name = 'post_publish_new'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name = 'add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 
 ]
